fadecaa97feb13b779fcd99464acb54a46ca8fd6
Zach Kellerman <63438523+zkellerman@users.noreply.github.com>
2024-09-03 10:13:04 -0400

    Created a method that will play a game. The game will continue until a score of 3 is reached.

--- scripts.js
+++ scripts.js
@@ -33,62 +33,86 @@ function getPlayerChoice() {
   }
 }
 
-function playRound(playerChoice, computerChoice) {
-  console.log(`You chose: ${playerChoice}`);
-  console.log(`Computer chose: ${computerChoice}`);
-
-  if (playerChoice == computerChoice) {
-    console.log("It's a tie! Replay the round.");
-    return;
+function playGame() {
+  function playRound(playerChoice, computerChoice) {
+    console.log(`You chose: ${playerChoice}`);
+    console.log(`Computer chose: ${computerChoice}`);
+  
+    if (playerChoice == computerChoice) {
+      console.log("It's a tie! Replay the round.");
+      return;
+    }
+  
+    switch (playerChoice) {
+      case 'Rock':
+        if (computerChoice == 'Paper') {
+          console.log('You lose! Paper beats Rock.');
+          computerScore++;
+        }
+        else if (computerChoice == 'Scissors') {
+          console.log('You win! Rock beats Scissors');
+          playerScore++;
+        }
+  
+        break;
+  
+      case 'Paper':
+        if (computerChoice == 'Rock') {
+          console.log('You win! Paper beats Rock.');
+          playerScore++;
+        }
+        else if (computerChoice == 'Scissors') {
+          console.log('You lose! Scissors beats Paper.');
+          computerScore++;
+        }
+  
+        break;
+  
+      case 'Scissors':
+        if (computerChoice == 'Rock') {
+          console.log('You lose! Rock beats Scissors');
+          computerScore++;
+        }
+        else if (computerChoice == 'Paper') {
+          console.log('You win! Scissors beats Paper');
+          playerScore++;
+        }
+  
+        break;
+  
+      default:
+        console.log('You entered an invalid choice. Replay the round.');
+        break;
+    }
   }
 
-  switch (playerChoice) {
-    case 'Rock':
-      if (computerChoice == 'Paper') {
-        console.log('You lose! Paper beats Rock.');
-        computerScore++;
-      }
-      else if (computerChoice == 'Scissors') {
-        console.log('You win! Rock beats Scissors');
-        playerScore++;
-      }
+  let playerScore = 0;
+  let computerScore = 0;
 
+  while (playerScore < 3 && computerScore < 3) {
+    const playerSelection = getPlayerChoice();
+    const computerSelection = getComputerChoice();
+    
+    playRound(playerSelection, computerSelection);
+    console.log('');
+
+    if (playerScore == 3 || computerScore == 3)
       break;
 
-    case 'Paper':
-      if (computerChoice == 'Rock') {
-        console.log('You win! Paper beats Rock.');
-        playerScore++;
-      }
-      else if (computerChoice == 'Scissors') {
-        console.log('You lose! Scissors beats Paper.');
-        computerScore++;
-      }
+    if (playerScore > computerScore)
+      console.log(`You're winning! The score is ${playerScore} - ${computerScore}.`);
+    else if (playerScore < computerScore)
+      console.log(`You're losing. The score is ${playerScore} - ${computerScore}.`);
+    else
+      console.log(`The score is tied ${playerScore} - ${computerScore}.`);
 
-      break;
-
-    case 'Scissors':
-      if (computerChoice == 'Rock') {
-        console.log('You lose! Rock beats Scissors');
-        computerScore++;
-      }
-      else if (computerChoice == 'Paper') {
-        console.log('You win! Scissors beats Paper');
-        playerScore++;
-      }
-
-      break;
-
-    default:
-      console.log('You entered an invalid choice. Replay the round.');
-      break;
+    console.log('');
   }
+
+  if (playerScore == 3)
+    console.log(`Congratulations! You won the game ${playerScore} - ${computerScore}.`);
+  else
+    console.log(`Better luck next time! You lost the game ${playerScore} - ${computerScore}.`);
 }
 
-let playerScore = 0;
-let computerScore = 0;
-
-const playerSelection = getPlayerChoice();
-const computerSelection = getComputerChoice();
-
-playRound(playerSelection, computerSelection);
+playGame();
