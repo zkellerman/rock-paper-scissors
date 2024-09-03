552f619971c4fc63359f02f237e0710a5b9c9e17
Zach Kellerman <63438523+zkellerman@users.noreply.github.com>
2024-09-03 09:49:42 -0400

    Created a function that will play a single round. The winner is announced and score is updated if no tie.

--- scripts.js
+++ scripts.js
@@ -33,5 +33,62 @@ function getPlayerChoice() {
   }
 }
 
+function playRound(playerChoice, computerChoice) {
+  console.log(`You chose: ${playerChoice}`);
+  console.log(`Computer chose: ${computerChoice}`);
+
+  if (playerChoice == computerChoice) {
+    console.log("It's a tie! Replay the round.");
+    return;
+  }
+
+  switch (playerChoice) {
+    case 'Rock':
+      if (computerChoice == 'Paper') {
+        console.log('You lose! Paper beats Rock.');
+        computerScore++;
+      }
+      else if (computerChoice == 'Scissors') {
+        console.log('You win! Rock beats Scissors');
+        playerScore++;
+      }
+
+      break;
+
+    case 'Paper':
+      if (computerChoice == 'Rock') {
+        console.log('You win! Paper beats Rock.');
+        playerScore++;
+      }
+      else if (computerChoice == 'Scissors') {
+        console.log('You lose! Scissors beats Paper.');
+        computerScore++;
+      }
+
+      break;
+
+    case 'Scissors':
+      if (computerChoice == 'Rock') {
+        console.log('You lose! Rock beats Scissors');
+        computerScore++;
+      }
+      else if (computerChoice == 'Paper') {
+        console.log('You win! Scissors beats Paper');
+        playerScore++;
+      }
+
+      break;
+
+    default:
+      console.log('You entered an invalid choice. Replay the round.');
+      break;
+  }
+}
+
 let playerScore = 0;
 let computerScore = 0;
+
+const playerSelection = getPlayerChoice();
+const computerSelection = getComputerChoice();
+
+playRound(playerSelection, computerSelection);
